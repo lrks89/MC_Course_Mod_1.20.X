aadadc3ceb302fc0b04b84e195b254d4acb50891
lrks_89 <kendoka89@gmail.com>
2023-11-26 13:01:41 -1000

    Added ToolTips

--- src/main/java/net/lrks_89/mccourse/block/custom/SoundBlock.java
+++ src/main/java/net/lrks_89/mccourse/block/custom/SoundBlock.java
@@ -2,6 +2,7 @@ package net.lrks_89.mccourse.block.custom;
 
 import net.lrks_89.mccourse.MCCourseMod;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -9,10 +10,16 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Properties pProperties) {
@@ -39,5 +46,11 @@ public class SoundBlock extends Block {
     public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
         pLevel.playSound(pEntity, pPos, SoundEvents.NOTE_BLOCK_BIT.get(), SoundSource.BLOCKS, 1f, 1f);
         super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.translatable("tooltip.mccourse.sound_block"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 }
